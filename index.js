64151c10d46b1f93167cec839009512537f3b952
Coy <patrick.bryan.peralta@gmail.com>
2019-04-01 11:40:53 -0400

    http://nicholasjohnson.com/node/course/exercises/modules/ Pt 2

--- index.js
+++ index.js
@@ -1,25 +1,12 @@
-const http = require('http');
 const Date = require('./DatePicker');
 const Router = require('./Router');
+const IndexController = require('./controllers/IndexController');
 
 console.log(new Date().today);
 
-http.createServer(function (request, response) {
-    let data = new Router().getRoute(request.url, (err, data) => {
-        if (err) {
-            response.writeHead(404);
-            response.write('<h1>Sorry Not Here!</h1>\n');
-            response.end();
+let controller = new IndexController(
+    new Router()
+);
 
-        } else {
-            response.writeHead(200);
-            response.write(data);
-            response.end();
-
-        }
-
-    });
-}).listen(3000);
-console.log('Server running at http://localhost:3000');
-
+controller.init();
 
