31acdd6d5f425b21df5b29245a097d77284f35ba
Coy <patrick.bryan.peralta@gmail.com>
2019-04-01 11:21:47 -0400

    http://nicholasjohnson.com/node/course/exercises/modules/ Pt 1

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 const http = require('http');
-const fs = require('fs');
+const Date = require('./DatePicker');
+const Router = require('./Router');
+
+console.log(new Date().today);
 
 http.createServer(function (request, response) {
-    fs.readFile(__dirname + '/views' + request.url + '.html', function (error, data) {
-        if (error) {
+    let data = new Router().getRoute(request.url, (err, data) => {
+        if (err) {
             response.writeHead(404);
             response.write('<h1>Sorry Not Here!</h1>\n');
             response.end();
-        } else {
 
+        } else {
             response.writeHead(200);
             response.write(data);
             response.end();
+
         }
+
     });
 }).listen(3000);
 console.log('Server running at http://localhost:3000');
